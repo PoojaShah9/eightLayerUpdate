639ff2ebd35bcdb57d79628526d9fa2d8f9c761f
PoojaShah9 <shah.developer9@gmail.com>
2018-10-10 13:14:28 +0530

    add changes

--- EightLayerApp/app/main.js
+++ EightLayerApp/app/main.js
@@ -65,11 +65,11 @@ function createWindow() {
 }
 
 // Create window on electron intialization
-// app.on('ready', createWindow()
-app.on('ready', function(){
-createWindow()
+app.on('ready', createWindow())
+// app.on('ready', function(){
+// createWindow()
   autoUpdater.checkForUpdatesAndNotify();
-});
+// });
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
 // On macOS specific close process
